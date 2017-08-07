c0fa8e2d53a489d7f42408eb17c41a3e3ede38dd
Zo1ton <ngageman@mail.ru>
2017-08-07 17:44:40 +0300

    0.0.0.0010

--- src/main/java/Tunes.java
+++ src/main/java/Tunes.java
@@ -4,7 +4,7 @@
 public enum Tunes {
     startjson ("window._sharedData = "),                    // С чего начинать искать json
     endjson (";</script>"),                                 // Чем заканчивается json. Искать только с места startjson
-    savedir("$temp$");                   // Здесь храним то, что пишем в файл. 10.07.2017 -  "d:\\Andrey\\Programming\\Java\\"
+    savedir("%TEMP%");                   // Здесь храним то, что пишем в файл. 10.07.2017 -  "d:\\Andrey\\Programming\\Java\\"
 
     private final String tune;
 
