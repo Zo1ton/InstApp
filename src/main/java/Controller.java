27141ec375c0db61d591be41ef0e8c54f8416d67
Zo1ton <ngageman@mail.ru>
2017-09-01 15:44:05 +0300

    0.0.0.0020

--- src/main/java/Controller.java
+++ src/main/java/Controller.java
@@ -1,7 +1,27 @@
 import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.Pane;
 
 public class Controller {
+    public TextArea info = new TextArea();
+    public Button getData = new Button();
+    public TextField login = new TextField();
+    public Pane mainPane;
+
+
+
     public void getInfoFromJson(ActionEvent actionEvent) {
+
+        getData.setOnAction(e -> {
+            String login1 =  login.getText();
+            if (!login1.isEmpty()) {
+                Person person = new Person(login1);          // Создаем новую запись пользователя
+                login.setText(person.getInfoAsString());
+            }
+            else login.setText("Введите логин!");
+        });
 
     }
 }
