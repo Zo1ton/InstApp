7b6cf7f5f633323bd949a75f357cd706bb494096
Andrey Vdovin <ngageman@mail.ru>
2017-09-07 00:12:14 +0300

    0.0.0.0027

--- src/main/java/Controller.java
+++ src/main/java/Controller.java
@@ -1,9 +1,13 @@
+import javafx.collections.FXCollections;
+import javafx.collections.ListChangeListener;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
 
 public class Controller implements Initializable {
@@ -12,11 +16,13 @@ public class Controller implements Initializable {
     public void initialize(URL location, ResourceBundle resources) {
     }
 
-    @FXML
-    private TextField textField;
+    List<Person> list = new ArrayList<>();
+    ObservableList<Person> ol = FXCollections.observableList(list);
 
-    @FXML
-    private Label label;
+    @FXML private TextField textField;
+    @FXML private Label label;
+    @FXML private TableView tableView;
+    @FXML private TableColumn id;
 
     @FXML
     public void pressOnButton() {
@@ -26,10 +32,17 @@ public class Controller implements Initializable {
             Person person = new Person(login);          // Создаем новую запись пользователя
             if (person.isExist == true) {
                 label.setText(person.getInfoAsString());
-//                list.add(person);
-//                System.out.println("В коллекцию добавленна запись " + person.getUserName() + " - id - " + person.getId());
+                list.add(person);
+                System.out.println("В коллекцию добавленна запись " + person.getUserName() + " - id - " + person.getId());
             }
         }
         else label.setText("Введите логин!");
     }
+
+    ol.addListener(new ListChangeListener<Person>() {
+        @Override
+        public void onChanged(Change<? extends Person> c) {
+        System.out.println("работает");
+            }
+        });
 }
