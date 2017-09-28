59a59bd6d49d2be8d4ce155301f58fa9c1254714
Zo1ton <ngageman@mail.ru>
2017-09-28 17:49:11 +0300

    0.0.0.0046

---
+++ src/test/java/PersonTest.java
@@ -0,0 +1,19 @@
+import objects.Person;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+public class PersonTest {
+
+    @Test
+    public void testGetId(){
+
+        String login = "ngageman61";
+        Person person = new Person(login, false);
+
+        assertEquals(person.getId(), 1418547643L);
+        assertEquals(person.getUserName(), login);
+        assertEquals(person.isExist(), true);
+    }
+}
