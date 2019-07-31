e1affa834b110f46a638ffaa0aae7316a065e5e5
Zo1ton <ngageman@mail.ru>
2019-07-31 17:00:57 +0300

    0.0.0.0088 - download followers feauters

--- src/main/java/controllers/DownloadFollowersFromListController.java
+++ src/main/java/controllers/DownloadFollowersFromListController.java
@@ -6,6 +6,9 @@ import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 import org.apache.log4j.Logger;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class DownloadFollowersFromListController extends BaseController {
 
     private static final Logger LOG = Logger.getLogger(DownloadFollowersFromListController.class);
@@ -16,9 +19,20 @@ public class DownloadFollowersFromListController extends BaseController {
     @FXML private Button btnDownload;
 
     @FXML private void download() {
-        String userList = textArea.getText();
-        LOG.info("userList:" + userList);
+        List<String> users = getListOfUsers(textArea.getText());
+        LOG.info("userList:" + users);
         LOG.info("user:" + userId);
         db.getMap().get(userId).get(db.getMap().get(userId).size() - 1).getMapFollowedBy();
+    }
+
+    private List<String> getListOfUsers(String textArea) {
+        List<String> list = new ArrayList<>();
+        String[] strings = textArea.split("\n");
+        for (String name : strings) {
+            if (name.length() > 0) {
+                list.add(name);
+            }
+        }
+        return list;
     }
 }
