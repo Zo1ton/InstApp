317e6d1fa6776467e45f19a41418f89e4e94616b
Andrey Vdovin <ngageman61@gmail.com>
2020-01-09 00:04:35 +0300

    0.0.0.0096 - доработал сравнение списка подписчиков (PersonMatcher)

--- src/main/java/logic/PersonMatcher.java
+++ src/main/java/logic/PersonMatcher.java
@@ -5,6 +5,7 @@ import objects.Person;
 import org.apache.log4j.Logger;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class PersonMatcher extends BaseController {
 
@@ -32,15 +33,15 @@ public class PersonMatcher extends BaseController {
     }
 
     public void matchPerson(long userId) {
-        List<String> after = getLists(userId, "Thu Oct 17 19:37:50 MSK 2019");
-        List<String> before = getLists(userId, "Wed Jul 31 18:39:59 MSK 2019");
+        List<String> after = getLists(userId, 1578229931148L);  //после
+        List<String> before = getLists(userId, 1578491404442L); //до
         matchLists(before, after);
     }
 
-    private List<String> getLists(long userId, String date) {
-        LOG.info("Запустили метод getLists2");
+    private List<String> getLists(long userId, Long date) {
+        LOG.info("Запустили метод getLists");
         List<String> list = new ArrayList<>();
-        Map<String, List<String>> allFollowersLists = getAllFollowersLists(userId);
+        Map<Long, List<String>> allFollowersLists = getAllFollowersLists(userId);
         if (allFollowersLists.isEmpty() || allFollowersLists.size() < 2) {
             LOG.info("Меньше двух списков");
         } else {
@@ -54,46 +55,43 @@ public class PersonMatcher extends BaseController {
         return list;
     }
 
-    private Map<String, List<String>> getAllFollowersLists(long userId) {
+    private Map<Long, List<String>> getAllFollowersLists(long userId) {
         List<Map<Date, Map<Long, String>>> list = getAllMapFollowsThisPerson(userId);
-        Map<String, List<String>> map = getMap(list);
-        map.forEach((date, listPerson) -> {
-            LOG.info(date + " " + listPerson.size());
+        Map<Long, List<String>> map = getMap(list);
+        TreeMap<Long, List<String>> sorted = new TreeMap(map);
+        sorted.forEach((date, listPerson) -> {
+            LOG.info(date + " " + listPerson.size() + " -- " + new Date(date));
         });
         return getMap(list);
     }
 
     /**
      * На вход подаем id пользователя
-     * На выходе получаем коллекцию пользователей
+     * На выходе получаем коллекцию пользователей, это история состояний этого пользователя.
      */
     private List<Map<Date, Map<Long, String>>> getAllMapFollowsThisPerson(long userId) {
-        List<Map<Date, Map<Long, String>>> list = new ArrayList<>();
-        for (Person person : db.getMap().get(userId)) {
-            list.add(person.getMapFollows());
-        }
-        return list;
+        db.getMap().get(userId);
+        return db.getMap().get(userId).stream()
+                .map(Person::getMapFollows)
+                .collect(Collectors.toList());
     }
 
-    private Map<String, List<String>> getMap(List<Map<Date, Map<Long, String>>> list) {
-        Map<String, List<String>> map = new HashMap<>();
+    private Map<Long, List<String>> getMap(List<Map<Date, Map<Long, String>>> list) {
+        Map<Long, List<String>> map = new HashMap<>();
         for (Map<Date, Map<Long, String>> mapMap : list) {
             if (mapMap != null) {
                 for (Map.Entry<Date, Map<Long, String>> entry : mapMap.entrySet()) {
-                    map.put(entry.getKey().toString(), getListFollowers(entry.getValue()));
+                    map.put(entry.getKey().getTime(), getListFollowers(entry.getValue()));
                 }
             }
         }
         return map;
     }
 
-    private List<String> getListFollowers(Map<Long, String> longStringMap) {
-        List<String> list = new ArrayList<>();
-        for (Map.Entry<Long, String> map : longStringMap.entrySet()) {
-            list.add(map.getValue());
-        }
-        return list;
+    /**
+     * Из мапы возвращаем только values в виде ArrayList
+     */
+    private List<String> getListFollowers(Map<Long, String> map) {
+        return new ArrayList<>(map.values());
     }
-
-
 }
